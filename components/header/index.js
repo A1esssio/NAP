27558f92d9caacf5161526286859f3e19e317e6b
A1esssio <your-email@associated-with-A1esssio.com>
2026-04-30 17:04:46 +0300

    delete home btn

--- components/header/index.js
+++ components/header/index.js
@@ -1,29 +1,25 @@
 export class HeaderComponent {
-    constructor(parent) {
-        this.parent = parent;
-    }
+  constructor(parent) {
+    this.parent = parent;
+  }
 
-    addListeners(onHomeClick) {
-        document
-            .getElementById('home-button')
-            .addEventListener('click', onHomeClick);
-    }
+  addListeners(onHomeClick) {}
 
-    getHTML() {
-        return `
+  getHTML() {
+    return `
         <nav class="navbar navbar-dark spacex-header sticky-top">
             <div class="container-fluid px-4">
-                <span class="navbar-brand header-logo mb-0">SPACE<span class="text-secondary">X</span> ARCHIVE</span>
-                <button id="home-button" class="btn btn-outline-secondary btn-sm btn-home">
-                    &#8592; HOME
-                </button>
+                <div class="logo">
+                    <a href="index.html">
+                        <img src="image/SpaceX_Logo_Black.png" alt="SPACEX" />
+                    </a>
+                </div>
             </div>
         </nav>
         `;
-    }
+  }
 
-    render(onHomeClick) {
-        this.parent.insertAdjacentHTML('beforeend', this.getHTML());
-        this.addListeners(onHomeClick);
-    }
+  render(onHomeClick) {
+    this.parent.insertAdjacentHTML("beforeend", this.getHTML());
+  }
 }
